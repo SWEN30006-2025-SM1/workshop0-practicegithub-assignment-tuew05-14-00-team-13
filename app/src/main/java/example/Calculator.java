d1edef36ef9caae3c59731b17660799214f94714
Sonicaii <41628956+Sonicaii@users.noreply.github.com>
2025-03-04 15:42:33 +1100

    Remove @todo comments

--- app/src/main/java/example/Calculator.java
+++ app/src/main/java/example/Calculator.java
@@ -2,17 +2,14 @@ package example;
 
 public class Calculator {
     public int add(int a, int b) {
-        // TODO: Implement this method
         return a+b;
     }
 
     public int minus(int a, int b) {
-        // TODO: Implement this method
         return a - b;
     }
 
     public int multiply(int a, int b) {
-        // TODO: Implement this method
         return a * b;
     }
 
